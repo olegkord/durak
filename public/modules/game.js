2b205bc4a7e99d9707b1ee24f629c2d592f8dc2d
Oleg <oleg.kord@gmail.com>
2015-12-03 20:40:53 -0500

    in process, writing attack game rules

--- public/modules/game.js
+++ public/modules/game.js
@@ -6,6 +6,9 @@ function Game(){
   this.deck = undefined;
   this.trump = undefined;
 
+  this.numOnField = [];
+  this.numPairs = 0;
+
   this.player1 = {};
   this.player2 = {};
 
@@ -55,7 +58,32 @@ function Game(){
   }
 
   this.makeAttack = function($card){
+    //expected input: jquery selector representing a card.
+    //attacking player plays a card.
+    //field must be empty OR have a card of the same number as this card.
+    let cardRank = parseInt($card.attr('data-value'));
+    if (_.isEmpty(this.numOnField)|| _.contains(this.numOnField, cardRank)) {
+      //if the field is empty OR has a card of the same number
 
+      //add card to collection of cards.
+      this.numOnField.push(cardRank);
+
+      //create a new field to play on.
+      let cardSuit = $card.attr('class');
+      cardSuit = _.last(cardSuit.split(' '));
+
+    }
+    else {
+
+    }
+  }
+
+  this.makeDefend = function($card){
+
+  }
+  this.recover = function() {
+
+    this.nextTurn();
   }
 
 
