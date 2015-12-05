30a1425f7f1b0f617c2f7adfb78aba757e7468fd
Oleg <oleg.kord@gmail.com>
2015-12-05 14:22:19 -0500

    going to host game on server

--- public/js/app.js
+++ public/js/app.js
@@ -8,16 +8,17 @@ let myUser = '';
 $('#user-login').click( (event) => {
   console.log('clicked login');
   let userName = $('input').val();
+  myUser = userName;
   socket.emit('user added', userName);
-
 })
 
 socket.on('two players', (players) => {
   console.log('response received');
   console.log(players);
   $('.login').hide();
+  $('#your-user').html(myUser)
 
-  
+
 })
 
 
