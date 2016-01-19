adcf7fbde61689aa5603ea405fc57dd5969474ff
Oleg <oleg.kord@gmail.com>
2016-01-19 17:48:46 -0500

    added socket factory

--- public/js/factories/socket.js
+++ public/js/factories/socket.js
@@ -1,6 +1,28 @@
-function Socket() {
+'use strict';
 
+function Socket($rootScope) {
 
+  let socket = io.connect(process.env.PORT || 'https://localhost:3000')
+  return {
+    on: function(eventName, callback) {
+      socket.on(eventName, () => {
+        //this is questionable.
+        let args = arguments;
+        $rootScope.$apply( () => {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function(eventName, data, callback) {
+      socket.emit(eventName, data, () => {
+      let args = arguments;
+      $rootScope.$apply( () => {
+        if (callback) {
+          callback.apply(socket, args);
+        }
+      });
+    });
+  }
 }
 
 module.exports = Socket;
