22ac5cbb90af5367cf228b1b3dad94329083dfff
Oleg <oleg.kord@gmail.com>
2016-01-28 18:06:59 -0500

    added framework for profile controller

--- public/js/controllers/profilecontroller.js
+++ public/js/controllers/profilecontroller.js
@@ -5,10 +5,25 @@ function ProfileController(User, $state, Socket){
 
   self.currentUser = User.getCurrentUser();
 
-  // self.getCurrentUser = function() {
-  //   return User.getCurrentUser();
-  // }
+  self.searchUsers = function() {
+    //a user searches for another user's email and can alert them.
 
+  }
+
+  self.selectGame = function() {
+    //a user clicks on a game, and it sends a notification to another user.
+
+  }
+
+  self.startGame = function() {
+    //a user can start a new game instance
+
+  }
+
+  self.editUser = function() {
+    //a user can choose to edit profile --> maybe make this a ui-sref
+    
+  }
 }
 
 module.exports = ProfileController;
